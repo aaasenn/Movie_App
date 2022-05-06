2b3bc68d44145df644e9051d7d4a03d0dbd3f36f
Макс Доши <johnsmalest@gmail.com>
2022-05-06 19:23:39 +0300

    solve auth

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,10 +11,11 @@ export default function Form({title, handleClick}) {
   });
 
   return (
-    <form className="container mt-4" onSubmit={handleSubmit((data) => {
-      setEmail(data.email)
-      setPass(data.pass)
-    })}>
+    <form className="container mt-4" onSubmit = {handleSubmit((data, e) => {
+      handleClick(email, pass)
+})
+      
+      }>
       <div className="form-group">
         <label for="exampleInputEmail1">Username</label>
 
@@ -41,8 +42,13 @@ export default function Form({title, handleClick}) {
          "Pass should be at least 6 length and includes numeric and Uppercase"}</p>
 
       </div>
-      {isValid ? (<button type="submit" onClick={() => handleClick(email, pass)} className="btn btn-lg btn-primary pr-4 pl-4 submit">{title}</button>) : 
-      (<button type="button" class="btn btn-lg btn-primary" disabled>{title}</button>
+      {isValid ? (<button type="submit" onClick = {() => {
+        const login = document.getElementById('exampleInputEmail1')
+        const password = document.getElementById('exampleInputPassword1')
+        setEmail(login.value)
+        setPass(password.value)
+        }} className="btn btn-lg btn-primary pr-4 pl-4 submit">{title}</button>) : 
+      (<button type="button" className="btn btn-lg btn-primary" disabled>{title}</button>
  )}
     </form>
   )
